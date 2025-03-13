362b88f8cf87a34c475630b822b419c49460c833
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 19:14:25 +0800

    storing inputs for program

--- Batch 3/get_numbers_with_no_duplicate.py
+++ Batch 3/get_numbers_with_no_duplicate.py
@@ -1,3 +1,12 @@
 #get 10 user inputs
+def get_no_duplicate_numbers():
+    numbers = []
+
+    print("Enter 10 numbers")
+    for i in range(10):
+        num = int(input("Enter number: "))
+        numbers.append(num)
+
 #find numbers with no duplicates
+        no_duplicate_numbers =
 #print the numbers without duplicates
