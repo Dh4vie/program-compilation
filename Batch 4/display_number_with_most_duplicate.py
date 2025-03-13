0a5222e7aa97d1fb3ab35d8115bc49e09a6c470c
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 23:57:18 +0800

    finished program 2

--- Batch 4/display_number_with_most_duplicate.py
+++ Batch 4/display_number_with_most_duplicate.py
@@ -1,3 +1,18 @@
 #ask user to input until invalid
+numbers = []
+
+while True:
+    try: 
+        num = int(input("Enter a Number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+
 #find most duplicated nunber
+if numbers:
+    most_dupe = max(numbers, key = numbers.count)
+    
 #display most duplicated number
+    print(most_dupe)
+else:
+    print('no numbers were entered')
