e6eb70aaa286244f6d2202f4377dd339058cf3c2
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 22:20:49 +0800

    finished program 5

--- Batch 3/input_sort_low_to_high.py
+++ Batch 3/input_sort_low_to_high.py
@@ -1,3 +1,18 @@
 #get user inputs
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except ValueError:
+        break
+
 #sort inputs hughest to lowest
+if numbers:
+    numbers.sort()
+
 #print inputs
+    print(numbers)
+else:
+    print("no numbers were entered.")
