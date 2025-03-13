b5ac648b57aa9e35b6f093002df1d05c06252d65
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 20:31:14 +0800

    finishing program 3

--- Batch 3/tell_duplicate_and_unique_until_invalid_input.py
+++ Batch 3/tell_duplicate_and_unique_until_invalid_input.py
@@ -1,3 +1,16 @@
 #get inputs
+numbers = set()
+
+while True:
+    try:
+        num = int(input("enter a number: "))
 #print if number is duplicate or unique
+        if num in numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
+            numbers.add(num)
 #exit program if invalid input
+    except ValueError:
+        print("Exit..")
+        break
