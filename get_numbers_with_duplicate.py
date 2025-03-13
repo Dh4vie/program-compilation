43cc34fa1a5876bedb0f16f5e5b435c46c596831
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 23:11:46 +0800

    finished batch 5 program 1

--- get_numbers_with_duplicate.py
+++ get_numbers_with_duplicate.py
@@ -1,3 +1,16 @@
 #get user inputs
+numbers = []
+
+print("Enter 10 numbers")
+for i in range(10):
+    num = int(input("Enter a number:"))
+    numbers.append(num)
+
 #find numbers with duplicates
+num_with_duplicate = []
+for num in numbers:
+    if numbers.count(num) != 1:
+        num_with_duplicate.append(num)
+
 #print numebrs with duplicates
+print(num_with_duplicate)
