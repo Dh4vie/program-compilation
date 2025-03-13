e6ef6a3b4c15436db535775cd6ed75023a674d1d
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 19:57:22 +0800

    storing inputs for program 2

--- Batch 3/display_first_number_with_duplicate.py
+++ Batch 3/display_first_number_with_duplicate.py
@@ -1,3 +1,12 @@
 #get 10 numbers
+numbers = []
+num_with_dupe = set()
+
+print("Enter 10 numbers")
+for i in range(10):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+
 #find the first number of numbers with duplicates
+
 #print the first number of numbers with duplicates
