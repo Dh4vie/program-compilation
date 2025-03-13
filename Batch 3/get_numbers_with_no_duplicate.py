b50eab33423671be56d7b3e2853458c0496bd6e9
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 19:38:02 +0800

    finishing program 1

--- Batch 3/get_numbers_with_no_duplicate.py
+++ Batch 3/get_numbers_with_no_duplicate.py
@@ -8,5 +8,12 @@ def get_no_duplicate_numbers():
         numbers.append(num)
 
 #find numbers with no duplicates
-        no_duplicate_numbers =
+        no_duplicate_numbers = []
+        for num in numbers:
+            if numbers.count(num) == 1:
+                no_duplicate_numbers.append(num)
+
 #print the numbers without duplicates
+    print(no_duplicate_numbers)
+
+get_no_duplicate_numbers()
