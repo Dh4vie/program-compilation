5b0459fba6c9afbc250584e1420e04d16c25d14e
Dave Casinginan <davecasinginan@gmail.com>
2025-03-13 22:09:44 +0800

    finished program 4

--- Batch 3/lowest_input_display_until_invalid.py
+++ Batch 3/lowest_input_display_until_invalid.py
@@ -1,2 +1,16 @@
 #get user inputs and loop until invaild
+lowest_num = None
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        if lowest_num is None or num < lowest_num:
+            lowest_num = num
+    except ValueError:
+        break
+
 #print the lowest value
+if lowest_num is not None:
+    print("lowest input:", lowest_num)
+else:
+    print("no numbers were entered")
