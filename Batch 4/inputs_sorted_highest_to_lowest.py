60a205bc33773141ecd84176b4cd2337004be334
Dave Casinginan <davecasinginan@gmail.com>
2025-03-14 01:49:24 +0800

    program 4 finished

--- Batch 4/inputs_sorted_highest_to_lowest.py
+++ Batch 4/inputs_sorted_highest_to_lowest.py
@@ -1,3 +1,16 @@
 #get user inputs and loop until invalid
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a Number: "))
+        numbers.append(num)
+    except ValueError:
+        break
 #sort inputs highest to lowest
+if numbers:
+    numbers.sort(reverse=True)
 #print the sorted inputs
+    print(numbers)
+else:
+    print("no numbers were entered.")
