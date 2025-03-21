75055e233100d456bc508292fc0b692ae81c325b
Dave Casinginan <davecasinginan@gmail.com>
2025-03-21 19:07:37 +0800

    program 9 finished

--- Batch 5/pascal_case_name.py
+++ Batch 5/pascal_case_name.py
@@ -1,3 +1,8 @@
 #Ask user input
+name = input('Enter fullname(preferably in incorrect casing): ')
+
 #format to pascal case
+pascal_case_name = name.title().replace(" ", "")
+
 #print result
+print(pascal_case_name)
