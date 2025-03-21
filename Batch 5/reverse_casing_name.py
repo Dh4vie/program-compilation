5278f6859c0cc21cfebef72485e4ca156aace3f3
Dave Casinginan <davecasinginan@gmail.com>
2025-03-21 18:45:42 +0800

    finished program 5

--- Batch 5/reverse_casing_name.py
+++ Batch 5/reverse_casing_name.py
@@ -1,3 +1,8 @@
 #Ask user for input
+name = input('Enter fullname(in any casing): ')
+
 #reverse the casings of the name
+reverse_case_name = name.swapcase()
+
 #print result
+print(reverse_case_name)
