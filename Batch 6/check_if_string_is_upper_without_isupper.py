19700489b97bbd6764a13bea79ff5ce963d94035
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 13:40:18 +0800

    program 4 finished

--- Batch 6/check_if_string_is_upper_without_isupper.py
+++ Batch 6/check_if_string_is_upper_without_isupper.py
@@ -1,3 +1,9 @@
 #create a string
+statement = str(input("Make any statement: "))
+
 #check if string is upper without using .isupper
+def isupper_replacement(string):
+    return string == string.upper()
+
 #print if true or false
+print(isupper_replacement(statement))
