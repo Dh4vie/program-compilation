b424494266e6205e2deddd4a5cddf8768ca10c5f
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 14:30:23 +0800

    program 6 finished

--- Batch 6/ljust_without_using_ljust.py
+++ Batch 6/ljust_without_using_ljust.py
@@ -1,3 +1,9 @@
 #create a string
+statement = str(input("Make any statement: "))
+
 #set a specific number of spaces to fit a certain parameter
+def lstring_replacement(string):
+    return string if len(string) >= 10 else string + " " * (10 - len(string))
+
 #print output
+print("'" + lstring_replacement(statement) + "'")
