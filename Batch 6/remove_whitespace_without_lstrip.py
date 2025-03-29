d481b282b907f52ddcacc3036bc6c8750f671c3b
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 11:14:44 +0800

    finished program 1

--- Batch 6/remove_whitespace_without_lstrip.py
+++ Batch 6/remove_whitespace_without_lstrip.py
@@ -1,2 +1,6 @@
 #make a string with spaces in the beginning
+statement = "                    This is a sentence"
+
 #remove the spaces in the beginning and print
+statement_fix = statement.strip()
+print(statement_fix)
