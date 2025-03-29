74016fb448cf2b44e170c18afd6bcfae76be42d9
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 12:11:14 +0800

    programt 3 finished

--- Batch 6/lowercase_string_without_lower,py.py
+++ Batch 6/lowercase_string_without_lower,py.py
@@ -1,2 +1,6 @@
 #create a string
+statement = str(input("make any statement to convert into lowercase: "))
+
 #convert the string to lowercase without using lower
+statement_fix = statement.casefold()
+print(statement_fix)
