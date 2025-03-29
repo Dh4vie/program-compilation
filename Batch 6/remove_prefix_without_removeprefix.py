54681ab68fa1d330669d518f73e1da35420951aa
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 11:34:41 +0800

    program 2 finished

--- Batch 6/remove_prefix_without_removeprefix.py
+++ Batch 6/remove_prefix_without_removeprefix.py
@@ -1,2 +1,6 @@
 #create a string
+statement = str(input("make a statement that starts with 'hello': "))
+
 #remove the prefix without using removeprefix
+staement_fix = statement.replace("hello", "").lstrip()
+print(staement_fix)
