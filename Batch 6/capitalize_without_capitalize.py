2ee7779baee02d6aa5c39242c95c9debd0d822c4
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 15:04:49 +0800

    program 9 finished

--- Batch 6/capitalize_without_capitalize.py
+++ Batch 6/capitalize_without_capitalize.py
@@ -1,3 +1,8 @@
 #crete a string
+statement = str(input("Input any statement: "))
+
 #capitalize the string without .capitalize
+statement_capitalized = statement[0].upper() + statement[1:].lower()
+
 #print output
+print(statement_capitalized)
