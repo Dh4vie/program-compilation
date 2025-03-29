ea839e6e8dcb8f94eabf9fad702a3b3a608d9bc1
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 14:45:01 +0800

    program 7 finished

--- Batch 6/center_without_center.py
+++ Batch 6/center_without_center.py
@@ -1,3 +1,14 @@
 #create a string
+statement = str(input("Input any statement/Word: "))
+
 #set a parameter to center the string
+def center_replacement(string):
+    if len(string) >= 15:
+        return string
+    total_spaces = 15 - len(string)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    return " " * left_spaces + string + right_spaces * " "
+
 #print output
+print("'" + center_replacement(statement) + "'")
