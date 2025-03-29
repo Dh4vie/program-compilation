ba5442190bb241a135ec4ea413e972520a80cc5a
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 15:20:26 +0800

    program 10 finished

--- Batch 6/capitalize_words_without_title.py
+++ Batch 6/capitalize_words_without_title.py
@@ -1,3 +1,8 @@
 #create a string
+statement = str(input("Make any statement: "))
+
 #capitalize every word without using .title
+title_case_statement = " ".join(word.capitalize() for word in statement.split())
+
 #print output
+print(title_case_statement)
