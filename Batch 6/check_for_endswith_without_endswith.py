78ae0e0a685e3d6b33c5ed61437c6f18526e782c
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 14:14:55 +0800

    program 5 finished

--- Batch 6/check_for_endswith_without_endswith.py
+++ Batch 6/check_for_endswith_without_endswith.py
@@ -1,3 +1,10 @@
 #create a string 
+statement = str(input("Make any statement: ")).rstrip()
+suffix = "anything"
+
 #check if word ends with parameter without endswith
+def endswith_replacement(string, suffix):
+    return string[-len(suffix):] == suffix if len(suffix) <= len(string) else False
+
 #print if true or false
+print(endswith_replacement(statement, suffix))
