851cdc0593fc1a3fa58e42dae5aea288331bee91
Dave Casinginan <davecasinginan@gmail.com>
2025-03-29 14:56:19 +0800

    program 8 finished

--- Batch 6/swap_casings_without_swapcase.py
+++ Batch 6/swap_casings_without_swapcase.py
@@ -1,3 +1,17 @@
 #create a string
+statement = str(input("Make any statement: "))
+
 #reverse casing without swapcase
+def swapcase_replacement(string):
+    output = ""
+    for letter in string:
+        if letter.islower():
+            output += letter.upper()
+        elif letter.isupper():
+            output += letter.lower()
+        else:
+            output += letter
+    return output
+
 #print output
+print(swapcase_replacement(statement))
