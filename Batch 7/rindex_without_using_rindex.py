ccb968aef6892fe1783109a35560419f78e59888
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 03:39:10 +0800

    program 10 finished

--- Batch 7/rindex_without_using_rindex.py
+++ Batch 7/rindex_without_using_rindex.py
@@ -1,3 +1,16 @@
 #create a string
-##create a certain paramter and make it function similar to rindex
+statement = str(input("Enter a statement: "))
+char_find = 'a'
+
+#create a certain paramter and make it function similar to rindex
+def rindex_replacement(string, search_char):
+    for i in range(len(string) - len(search_char), -1, -1):
+        if string[i:i+len(search_char)] == search_char:  
+            return i  
+    raise ValueError(f"'{search_char}' not found in the string")
+
 #print output
+try:
+    print(rindex_replacement(statement, char_find))
+except ValueError as e:
+    print(e)
