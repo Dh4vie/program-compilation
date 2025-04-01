fa2a40be0c5b9c619f95d7325c3eeb4625460cb0
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 00:04:30 +0800

    fixed batch 6 program 2 and finished batch 7 program 2

--- Batch 6/remove_prefix_without_removeprefix.py
+++ Batch 6/remove_prefix_without_removeprefix.py
@@ -2,5 +2,8 @@
 statement = str(input("make a statement that starts with 'hello': "))
 
 #remove the prefix without using removeprefix
-staement_fix = statement.replace("hello", "").lstrip()
-print(staement_fix)
+if statement.startswith("hello"):
+    statement_fix = statement[5:].lstrip()
+else: 
+    statement_fix = statement
+print(statement_fix)
--- Batch 7/remove_suffix_without_removesuffix.py
+++ Batch 7/remove_suffix_without_removesuffix.py
@@ -1,3 +1,11 @@
 #create string
+statement = str(input("Make a statement that ends with 'bye': "))
+
 #remove suffix without .removesuffix
+if statement.endswith("bye"):
+    statement_fix = statement[:-3].rstrip()
+else:
+    statement_fix = statement
+
 #print outputs
+print(statement_fix)
