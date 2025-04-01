76c155f5d4a6d925d51ada460871d7582fc16e5b
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 03:05:30 +0800

    program 7 finished

--- Batch 7/fill_zeros_without_zfill.py
+++ Batch 7/fill_zeros_without_zfill.py
@@ -1,3 +1,9 @@
 #create string
+statement = str(input("Enter a statement: "))
+
 #fill desired length with zeros without zfill
+def zfill_replacement(string):
+    return "0" * (10 - len(string)) + string if len(string) < 10 else string
+
 #print output
+print(zfill_replacement(statement))
