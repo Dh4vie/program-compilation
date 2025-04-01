cd798292f258293cda712734b0f113cd53458835
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 02:29:28 +0800

    program 5 finished

--- Batch 7/check_for_startswith_without_startswith.py
+++ Batch 7/check_for_startswith_without_startswith.py
@@ -1,3 +1,10 @@
 #create string
+statement = str(input("Make any statement: ")).rstrip()
+prefix = "hello"
+
 #check if word ends with parameter without endswith
+def startswith_replacement(string, prefix):
+    return string[:len(prefix)] == prefix if len(prefix) <= len(string) else False
+
 #print if true or false
+print(startswith_replacement(statement, prefix))
