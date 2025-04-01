3292d092f8caea72fffef3b12a74f43d40d805d6
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 03:14:50 +0800

    programg 8 finished

--- Batch 7/count_without_using_count.py
+++ Batch 7/count_without_using_count.py
@@ -1,3 +1,14 @@
 #create string
+statement = str(input("Enter a statement: "))
+counted_character = "a"
+
 #count a certain parameter
+def count_replacement(string, count_char):
+    count = 0
+    for i in range(len(string) - len(count_char) + 1):  
+        if string[i:i+len(count_char)] == count_char:  
+            count += 1
+    return count
+
 #print output
+print(count_replacement(statement, counted_character)) 
