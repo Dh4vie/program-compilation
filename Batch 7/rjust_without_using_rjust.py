f4e5877c4d02793d1967a4379fa4bbab051455e6
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 02:37:08 +0800

    program 6 finished

--- Batch 7/rjust_without_using_rjust.py
+++ Batch 7/rjust_without_using_rjust.py
@@ -1,3 +1,9 @@
 #create string
+statement = str(input("Enter a statement: "))
+
 #set a specific number of spaces to fit a certain parameter
+def rjust_replacement(string):
+    return " " * (15 - len(string)) + string if len(string) < 15 else string
+
 #print output
+print(rjust_replacement(statement))
