b84b469b5dff90f3627eda23757ab41e54317e59
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 03:32:55 +0800

    program 9 finished

--- Batch 7/use_index_without_using_index.py
+++ Batch 7/use_index_without_using_index.py
@@ -1,3 +1,15 @@
 #create string 
+statement = str(input("Enter a statement: "))
+char_find = 'a'
+
 #create a certain paramter and make it function similar to index
+def index_replacement(string, search_char):
+    for i in range(len(string) - len(search_char) + 1):
+        if string[i:i+len(search_char)] == search_char:
+            return i
+    raise ValueError(f"'{search_char}' not found in the string")
 #print output
+try:
+    print(index_replacement(statement, char_find))
+except ValueError as e:
+    print(e)
