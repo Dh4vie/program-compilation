c701bd0b062e37eee2dea168122e7e875404c3a2
Dave Casinginan <davecasinginan@gmail.com>
2025-04-01 23:48:42 +0800

    program 1 finished

--- Batch 7/remove_space_without_rstrip.py
+++ Batch 7/remove_space_without_rstrip.py
@@ -1,3 +1,12 @@
 #create a string
+statement = str(input("Input any statement with spaces on end: "))
+
 #remove spaces
+def remove_end_space(string):
+    index = len(string) - 1
+    while index >= 0 and string[index] == ' ':
+        index -= 1
+    return string[:index+1]
+
 #print output
+print(remove_end_space(statement))
