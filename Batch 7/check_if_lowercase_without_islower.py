4fd558711cb1c42e1d0cbfc98c3c0ddbad85bd57
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 00:44:30 +0800

    program 4 finished

--- Batch 7/check_if_lowercase_without_islower.py
+++ Batch 7/check_if_lowercase_without_islower.py
@@ -1,3 +1,12 @@
 #create string
+statement = str(input("Make a statement: "))
+
 #check if string is lowercase without islower
+def islower_replacement(string):
+    for char in string:
+        if 'A' <= char <= 'Z':
+            return False
+    return True
+
 #print output
+print(islower_replacement(statement))
