f42752b7ff5d962f168b5433f2962bf82a5406ed
Dave Casinginan <davecasinginan@gmail.com>
2025-04-02 00:20:50 +0800

    program 3 finished

--- Batch 7/uppercase_without_upper.py
+++ Batch 7/uppercase_without_upper.py
@@ -1,3 +1,15 @@
-#create string 
+#create string
+statement = str(input("make any statement to convert into uppercase: "))
+
 #covert to uppercase without upper
+def upper_replacement(string):
+    result = ''
+    for char in string:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result
+    
 #print output
+print(upper_replacement(statement))
